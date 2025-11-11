9e7ba5db3edff1f4fd2729b93352598d4b2944b8
Lucas <lucas.fontana@sempreceub.com>
2025-11-11 10:17:15 -0300

    Create Turma.java

---
+++ Aula16/negocio/Turma.java
@@ -0,0 +1,54 @@
+package negocio;
+
+import persistencia.TurmaDAO;
+
+public class Turma {
+	//Class attributes
+	private int id = 0;
+	private Disciplina objDisciplina = null;
+	private Professor objProfessor = null;
+	private String letra = "";
+	
+	//Constructor
+	public Turma() {
+		super();
+	}
+	public Turma(int id, Disciplina objDisciplina, Professor objProfessor, String letra) {
+		super();
+		this.id = id;
+		this.objDisciplina = objDisciplina;
+		this.objProfessor = objProfessor;
+		this.letra = letra;
+	}
+	
+	//Access methods
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Disciplina getObjDisciplina() {
+		return objDisciplina;
+	}
+	public void setObjDisciplina(Disciplina objDisciplina) {
+		this.objDisciplina = objDisciplina;
+	}
+	public Professor getObjProfessor() {
+		return objProfessor;
+	}
+	public void setObjProfessor(Professor objProfessor) {
+		this.objProfessor = objProfessor;
+	}
+	public String getLetra() {
+		return letra;
+	}
+	public void setLetra(String letra) {
+		this.letra = letra;
+	}
+	
+	//
+	public void persistir() throws Exception {
+		new TurmaDAO().persistir(this);
+	}
+}
