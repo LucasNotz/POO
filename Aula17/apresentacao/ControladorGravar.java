6516d5188b6b0d4cc5041e8fea5cae2aa82331d0
Lucas <lucas.fontana@sempreceub.com>
2025-11-18 10:45:18 -0300

    Create ControladorGravar.java

---
+++ Aula17/apresentacao/ControladorGravar.java
@@ -0,0 +1,66 @@
+package apresentacao;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import negocio.Cliente;
+import negocio.Compra;
+import negocio.Produto;
+
+public class ControladorGravar implements ActionListener {
+	private JComboBox<String> cboProduto = null;
+	private JComboBox<String> cboCliente = null;
+	private JTextField txtData = null;
+	private JTextField txtQuantidade = null;
+	private Compra objCompra = null;
+	private JList<String> lstCompra = null;
+	
+	public ControladorGravar(JComboBox<String> cboProduto, JComboBox<String> cboCliente, JTextField txtData, JTextField txtQuantidade, JList<String> lstCompra) {
+		this.cboCliente = cboCliente;
+		this.cboProduto = cboProduto;
+		this.txtData = txtData;
+		this.lstCompra = lstCompra;
+		this.txtQuantidade = txtQuantidade;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (cboCliente.getSelectedIndex() == 0) {
+			JOptionPane.showMessageDialog(null, "Selecione um cliente");
+			return;
+		}
+		if (cboProduto.getSelectedIndex() == 0) {
+			JOptionPane.showMessageDialog(null, "Selecione um produto");
+			return;
+		}
+		if (txtData.getText().equals("")) {
+			JOptionPane.showMessageDialog(null, "Insira uma data");
+			return;
+		}
+		if (txtQuantidade.getText().equals("")) {
+			JOptionPane.showMessageDialog(null, "Insira uma quantidade");
+			return;
+		}
+		
+		try {
+			
+			objCompra = new Compra();
+			objCompra.setObjCliente(Cliente.getTodos().get(cboProduto.getSelectedIndex()-1));
+			objCompra.setObjProduto(Produto.getTodos().get(cboProduto.getSelectedIndex()-1));
+			objCompra.setData(txtData.getText());
+			objCompra.setQuantidade(Integer.parseInt(txtQuantidade.getText()));
+			objCompra.persistir();
+			JOptionPane.showMessageDialog(null, "Tudo certo");
+			
+		} catch (Exception error) {
+			error.printStackTrace();
+		}
+
+	}
+
+}
