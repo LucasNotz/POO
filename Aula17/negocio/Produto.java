d1c6c3e4dc4db39ff2057c90a27864a4a5556cb9
Lucas <lucas.fontana@sempreceub.com>
2025-11-18 10:52:15 -0300

    Create Produto.java

---
+++ Aula17/negocio/Produto.java
@@ -0,0 +1,56 @@
+package negocio;
+
+import java.util.ArrayList;
+
+import persistencia.ProdutoDAO;
+
+public class Produto {
+	//Propriedades da classe
+	private int id = 0;
+	private String nome = "";
+	private double preco = 0;
+
+	//Construtores
+	public Produto() {
+		super();
+	}
+
+	public Produto(int id, String nome, double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	//Getters & Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	//Métodos da classe
+	
+	public static ArrayList<Produto> getTodos() throws Exception {
+		return new ProdutoDAO().getTodos();
+	}
+	
+}
